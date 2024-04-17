f47c99ce6662c2d794055e917ebedf38df691b53
Pratik Mishra <pratikmishra141@gmail.com>
2024-04-17 22:31:24 +0530

    Create lecture_three.py

---
+++ Apan-College-python-classes/lecture_three.py
@@ -0,0 +1,41 @@
+# Lists in Python.
+
+# list = [2,3,5,6]
+# list.append(10)
+# list.sort(reverse=True)
+# list.reverse()
+# list.insert(2,7)
+# list.remove(5)
+# list.pop(1)
+# print("Appended value: " ,list)
+
+# fav_movies=[]
+# fav_movie = input(("Enter your favorite movie :"))
+# fav_movies.append(fav_movie)
+# fav1 = input("Enter your 1ST favorite movies : ")
+# fav_movies.append(fav1)
+# fav2 = input("Enter your 2ND favorite movies : ")
+# fav_movies.append(fav2)
+# fav3 = input("Enter your 3RD favorite movies : ")
+# fav_movies.append(fav3)
+
+# print(fav_movies)
+
+# ele = [1,2,3]
+
+# ele_copy = ele.copy()
+# ele_copy.reverse()
+
+
+# if(ele == ele_copy):
+#     print("Both lists are identical")
+# else:
+#     print("Lists are not identical")
+
+# tuple = ("C","D","A","A","B","B","A")
+# print(tuple.count("A"))
+
+
+# grade = ["C","D","A","A","B","B","A"]
+# grade.sort()
+# print(grade)
