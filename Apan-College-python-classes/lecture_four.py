46bf4cfb29b6f8b00dc665cb5e2949360b39b719
Pratik Mishra <pratikmishra141@gmail.com>
2024-04-17 22:31:12 +0530

    Create lecture_four.py

---
+++ Apan-College-python-classes/lecture_four.py
@@ -0,0 +1,67 @@
+# About Dictionary 
+
+# info = {
+#     "key" : "value",
+#     "name" : "pratik",
+#     "subject" : ["python","java","html"],
+#     "Topics" : ("dict","set"),
+#     "age" : 23
+# }
+# to create a null  dictionary we can write
+# null_dictionary = {}
+# print(null_dictionary)
+# To access the value of any key in the dictionary,we use following syntax:
+# print(type(info))
+# print(info["name"])
+# info["name"] = "Roy"
+# info["surname"] = "Mishra"
+# print(info)
+
+# print(info["name"])
+# print(info.get("name"))
+# info.update({"city":"Bangalore"})
+# print(info)
+
+# About Set
+# collection = {1,2,3,4}
+
+# print(collection.pop())
+# print(type(collection))
+# print(collection)
+
+# set1 = {1,2,3}
+# set2 = {2,3,4}
+
+# print(set1.union(set2))
+# print(set1.intersection(set2))
+
+# dictionary = {
+#     "table": ["a piece of furniture", "list of facts & figures"],
+#     "cat": "a small animal"
+# }
+# print(dictionary)
+
+# sub1 = {"python", "java", "c++","python","javascript"}
+# sub2 = {"java","python","java","c++","C"}
+
+# reqsubject = sub1.union(sub2)
+# print(reqsubject)
+# print(len(reqsubject))
+
+# subject = {"python", "java", "c++","python","javascript"
+# "java","python","java","c++","C"}
+
+# print(len(subject))
+
+# marks = {}
+
+# x = int(input("enter phy : "))
+# marks.update({"phy": x})
+
+# y = int(input("enter phy : "))
+# marks.update({"math": y})
+
+# z = int(input("enter phy : "))
+# marks.update({"chem": z})
+
+# print(marks)
