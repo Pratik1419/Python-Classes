019ef8a9c16611467579fb6c96c9d9831fcd751d
Pratik Mishra <pratikmishra141@gmail.com>
2024-04-17 22:31:20 +0530

    Create lecture_six.py

---
+++ Apan-College-python-classes/lecture_six.py
@@ -0,0 +1,56 @@
+# about function and Recursion
+
+# def calc_sum(a,b):
+#     sum = a+b
+#     return sum
+
+# print(calc_sum(2,3))
+
+# def calc_avg(a, b, c):
+#     sum = a + b + c
+#     avg = sum / 3
+#     print(avg)
+#     return avg
+
+# calc_avg(1, 2, 3)
+
+# num = [5,4,6,89]
+# def find_list(num):
+#     print(num)
+
+# find_len(num)
+
+# num = [23,456,735,8874]
+
+# def find_list(list):
+#     for items in list:
+#         print(items, end=" ")
+
+# find_list(num)
+
+
+# def cal_fact(n):
+#     fact = 1
+#     for i in range(1, n+1):
+#         fact *= i
+#     print(fact)
+
+# cal_fact(3)
+
+# convert usd into RS
+
+# def converter(usd_val):
+#     inr_val = usd_val * 83
+#     print(usd_val, "USD =", inr_val, "INR")
+
+# converter(4)
+
+# n = 8
+
+# def find_odd_even(n):
+#     if n % 2 == 0:
+#         print("EVEN")
+#     else:
+#         print("ODD")
+
+# find_odd_even(n)
