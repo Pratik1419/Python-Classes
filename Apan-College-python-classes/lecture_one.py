ed7156e2d404c4d7c60daadc92cc97f1687b28a8
Pratik Mishra <pratikmishra141@gmail.com>
2024-04-17 22:31:17 +0530

    Create lecture_one.py

---
+++ Apan-College-python-classes/lecture_one.py
@@ -0,0 +1,27 @@
+# A = 10
+# B = 11
+# Sum = A + B
+# print("The sum of",A,B, "is", Sum)
+
+# name = input("Enter Your Name")
+# print("Welcome", name,"to the world of Python!")
+
+# num1 = int(input("Enter first number"))
+# num2 = int(input("Enter Secound number"))
+
+# sum = num1 + num2
+# print("The sum is: ", sum)
+
+# side = float(input("Enter the side of a square = "))
+# area = side * side
+# print("The area of the square is: ", area)
+
+# a = float(input("Enter the number = "))
+# b = float(input("Enter the number = "))
+# area = a+b/2.0
+# print("The average value is : ", area)
+
+# a = int(input("Enter first number = "))
+# b =  int(input("Enter second number = "))
+
+# print(a >= b)
