61236aca8c1eab14cd0e1f47b859f77cf7af58a2
Pratik Mishra <pratikmishra141@gmail.com>
2024-04-17 22:31:30 +0530

    Create lecture_two.py

---
+++ Apan-College-python-classes/lecture_two.py
@@ -0,0 +1,60 @@
+# str1 = "Hello"
+# len(str1)
+# print(len(str1))
+# str2 = "Bros"
+# final_str = str1 + str2
+# print(final_str)
+
+# str = "Pratik Mishra"
+# ch = str[3]
+# ch1 = str[0:7]
+# print(ch1)
+# print(ch)
+
+# str1 = "i am a coder"
+# str1 = str1.capitalize()
+# str1 = str1.replace("er","ing")
+# str1 = str1.count("a")
+# print(str1)
+
+
+# name = input("Enter your first name :")
+# print("length of your name  is ", len(name))
+
+
+# str = "yo$$bros$$"
+# str = str.count("$")
+# print(str)
+
+# age = 99
+
+# if(age >= 18):
+#     if(age >= 80):
+#         print("cannot drive")
+#     else:
+#         print("can drive")
+# else:
+#     print("cannot drive")
+
+# num = int(input("Enter the number: "))
+# if (num % 2 == 0):
+#     print(num,"is even")
+# else:
+#     print(num,"is odd")
+
+# a = int(input("Enter the 1st number: "))
+# b = int(input("Enter the 2nd number: "))
+# c = int(input("Enter the 3rd number: "))
+
+# if(a >= b and a >= c):
+#     print("first is greater", a)
+# elif(b >= c):
+#     print("second is greater", b)
+# else:
+#     print("third is greater", c)
+
+# num = int(input("Enter a number: "))
+# if(num % 7 == 0):
+#     print(num,"is divisible by 7.")
+# else:
+#     print(num, "is not divisible by 7.")
